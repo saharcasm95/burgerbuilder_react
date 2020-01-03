7e7bd088e827e7ffb176384b24d1d04e3849236a
Saharcasm <nooresahar95@gmail.com>
2020-01-03 22:10:12 +1100

    UI fixes

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -4,7 +4,7 @@ import classes from './Logo.css';
 
 const logo = (props) => (
     <div className={classes.Logo}>
-        <img src={burgerLogo} alt="MyBurger"/>
+        <img src={burgerLogo} style={{height: props.height}} alt="MyBurger"/>
     </div>
 );
 
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,7 +6,7 @@ import classes from "./SideDrawer.css"
 const sideDrawer = () => {
     return (
         <div className={classes.SideDrawer}>
-            <Logo/>
+            <Logo height="11%"/>
             <nav>
                 <NavigationItems/>
             </nav>
