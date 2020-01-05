da082dd5cd483e73b573c2e4468838935b993886
Saharcasm <nooresahar95@gmail.com>
2020-01-05 20:26:27 +1100

    backdrop added to sidedraer

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 import classes from "./SideDrawer.css"
-
+import Wrapper from '../../../hoc/Wrapper';
 const sideDrawer = () => {
     return (
-        <div className={classes.SideDrawer}>
-            <Logo height="11%"/>
-            <nav>
-                <NavigationItems/>
-            </nav>
+        <Wrapper>
+            <Backdrop show/>
+            <div className={classes.SideDrawer}>
+                <Logo height="11%"/>
+                <nav>
+                    <NavigationItems/>
+                </nav>
 
-        </div>
+            </div>
+
+        </Wrapper>
 
     );
 };
